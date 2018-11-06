9c4477f3236c35a63f563c8dd80dbf1d24631899
Andres2896 <44794819+Andres2896@users.noreply.github.com>
2018-11-06 02:10:25 -0600

    Pag

---
+++ js/jscript.js
@@ -0,0 +1,48 @@
+function grande1 (x)
+{ document.getElementById("aladino").poster="img/Aladin2.jpg";}
+
+function original1 (x)
+{ document.getElementById("aladino").poster="img/Aladin.jpeg";}
+
+function grande2 (x)
+{  document.getElementById("capitan").poster="img/capitan2.jpg";}
+
+function original2 (x)
+{document.getElementById("capitan").poster="img/capitan.jpeg";}
+
+
+function crucero1 (x)
+{document.getElementById("crucero").style.backgroundImage="url('img/crucero.jpg')"}
+
+
+function crucero2 (x)
+{document.getElementById("crucero").style.backgroundImage="url('img/dimg4.jpeg')"}
+
+
+function rusa1 (x)
+{document.getElementById("rusa").style.backgroundImage="url('img/dimg10.jpeg')"}
+
+
+function rusa2 (x)
+{document.getElementById("rusa").style.backgroundImage="url('img/dimg5.jpeg')"}
+
+
+
+function play_a (x)
+{document.getElementById("parque").style.backgroundImage="url('img/dimg11.jpeg')"; 
+document.getElementById("audio").play()}
+
+
+
+function pause_a (x)
+{document.getElementById("parque").style.backgroundImage="url('img/dimg6.jpeg')";
+document.getElementById("audio").pause()}
+
+
+
+
+
+
+
+
+
